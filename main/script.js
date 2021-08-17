fe6d9bf0568f24ec33a5baf1229093320dbfa420
Juan Vazquez <ehulises28@hotmail.com>
2021-08-17 12:48:53 -0700

    added if statements that compares computer chices and user choices to see who won

--- main/script.js
+++ main/script.js
@@ -1,26 +1,32 @@
+const wins = 0;
+const losses = 0;
+const ties = 0;
+const keepPlaying = true;
 // caputure user choice
-    //user can pick r,p, or s
-    const userChoice = prompt('what do you choose?r, p or s?')
-    console.log(userChoice);
-    //edge case:  user puts in capital letters
-    //edge case: user puts in invalid letter
-    //edge case: user cancels out of prompt
+//user can pick r,p, or s
+while (keepPlaying) {
+  const userChoice = prompt("what do you choose?r, p or s?");
+  console.log(userChoice);
 
-// generate computer choice   
- const choices = ["r","p","s"]
-    //3 possible chioces rock paper and scissors
-    const randomChoices = Math.floor(Math.random()*3)
-    const computerChoice = choices[randomChoices]
-
-console.log(computerChoice);
-    // should be randomized each time
-
-// TODO: compare choices
-    // rock beats paper, paper beats rock scissors beats paper.
-    //update stats each round
-
-// TODO: show current session stats
-    //track current users wins losses and ties
-    
-// TODO: ask to play again 
-    //if user says play again, start over. otherwise, stop
+  // generate computer choice
+  //3 possible chioces rock paper and scissors
+  const choices = ["r", "p", "s"];
+  // should be randomized each time
+  const randomChoices = Math.floor(Math.random() * 3);
+  const computerChoice = choices[randomChoices];
+  console.log(computerChoice);
+  alert(`you choose ${userChoice} and i choose ${computerChoice}`);
+  // compare choices
+  if (userChoice === computerChoice) {
+    alert("is a tie!!");
+  } else if (userChoice === "s" && computerChoice === "p") {
+    alert("its a win!");
+  } else if (userChoice === "p" && computerChoice === "r") {
+    alert("its a win!");
+  } else if (userChoice === "r" && computerChoice === "s") {
+    wi;
+  } else {
+    console.log("its a loss");
+  }
+}
+// rock beats paper, paper beats rock scissors beats paper.
