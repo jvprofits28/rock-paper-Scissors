a0b8d1ecb40f3f6b1a2bf7c1ac95ca0f285b5efe
Juan Vazquez <ehulises28@hotmail.com>
2021-08-17 13:08:47 -0700

    fixed some variables and score board is now working

--- main/script.js
+++ main/script.js
@@ -1,20 +1,15 @@
-const wins = 0;
-const losses = 0;
-const ties = 0;
-const keepPlaying = true;
+let wins = 0;
+let losses = 0;
+let ties = 0;
+let choices = ["r", "p", "s"];
+let keepPlaying = true;
 // caputure user choice
 //user can pick r,p, or s
 while (keepPlaying) {
-  const userChoice = prompt("what do you choose?r, p or s?").toLowerCase;
-  console.log(userChoice);
-
-  // generate computer choice
-  //3 possible chioces rock paper and scissors
-  const choices = ["r", "p", "s"];
+  const userChoice = prompt("what do you choose?r, p or s?").toLowerCase();
   // should be randomized each time
   const randomChoices = Math.floor(Math.random() * 3);
   const computerChoice = choices[randomChoices];
-  console.log(computerChoice);
   alert(`you choose ${userChoice} and i choose ${computerChoice}`);
   // compare choices
   if (userChoice === computerChoice) {
