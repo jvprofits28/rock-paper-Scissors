6edb712027492fdc2fc6667f9548633f006fa394
Juan Vazquez <ehulises28@hotmail.com>
2021-08-17 11:59:24 -0700

    added files

---
+++ main/script.js
@@ -0,0 +1,19 @@
+// TODO: caputure user choice
+    //user can pick r,p, or s
+    //edge case:  user puts in capital letters
+    //edge case: user puts in invalid letter
+    //edge case: user cancels out of prompt
+
+// TODO: generate computer choice
+    //3 possible chioces rock paper and scissors
+    // should be randomized each time
+
+// TODO: compare choices
+    // rock beats paper, paper beats rock scissors beats paper.
+    //update stats each round
+
+// TODO: show current session stats
+    //track current users wins losses and ties
+    
+// TODO: ask to play again 
+    //if user says play again, start over. otherwise, stop
